de456851336c262ae68e3dc3ee4766ec2e324cb6
kumar halder <mairalaamre@gmail.com>
2020-05-12 21:58:40 -0600

    Game mechanism completed, requires to add automated testing

--- board.py
+++ board.py
@@ -1,41 +1,76 @@
 class board:
-    def __init__(self,health=3):
-        self.ownShip = [[0 for i in range(8)] for j in range(8)]
+    def __init__(self,name, health=3):
+        #self.ownShip = [[0 for i in range(8)] for j in range(8)]
+        self.name = name
         self.opponentShip =  [[0 for i in range(8)] for j in range(8)]
         self.opponentBoard =  [[0 for i in range(8)] for j in range(8)]
-        self.health = health
+        self.opponentShipCoordinates = []
+        self.opponentHealth = health
 
     def placeShip(self,x:int,y:int,isHorizontal:bool):
         if type(x) is not int or type(y) is not int or type(isHorizontal) is not bool:
             print("invalid position, (x,y) values must be int, and isHorizontal should be boolean")
-            return
+            return False
         #ship placed horizontally
         if isHorizontal:
             if x > 6 or x < 1:
                 print("invalid position")
-                return
+                return False
             if y > 7 or y < 0:
                 print("invalid position")
-                return
+                return False
             for i in range(-1,2):
-                self.ownShip[y][x+i]='z'
+                self.opponentShip[y][x+i]='z'
+                self.opponentShipCoordinates.append([x+i,y])
         #ship placed vertically
         else:
             if x > 7 or x < 0:
                 print("invalid position")
-                return
+                return False
             if y > 6 or y < 1:
                 print("invalid position")
-                return
+                return False
             for i in range(-1,2):
-                self.ownShip[y+i][x]='z'
-
+                self.opponentShip[y+i][x]='z'
+                self.opponentShipCoordinates.append([x, y+i ])
+        print(self.opponentShipCoordinates)
+        return True
+    def isOpponentShipAtPosition(self,x,y):
+        for coordinate in self.opponentShipCoordinates:
+            if coordinate[0] == x and coordinate[1] == y :
+                return True
+        return False
+    def isThisMoveUSedPreviously(self,x,y):
+        if (self.opponentBoard[y][x] != 0 ):
+            return True
+        return False
     def shotPoint(self,x,y):
-        # if (self.opponentBoard[y][x] != 0):
-        #     return "try again"
-        self.opponentBoard[y][x] = 1
+        #check if move is invalid
+
+        #miss
+        if self.isOpponentShipAtPosition(x,y) == False:
+            print("miss")
+            self.opponentBoard[y][x] = "miss"
+
+        #Hit
+        else:
+            print("hit")
+            self.opponentBoard[y][x] = "hit"
+            self.opponentBeenShot()
+
+
         print(self.opponentBoard)
-    def beenShot(self):
-        self.health -=1
-        print(self.health)
+
+    def opponentBeenShot(self):
+        self.opponentHealth -= 1
+        #print("health: ", self.opponentHealth)
+
+
+    def opponentDestroyed(self):
+        if (self.opponentHealth <= 0):
+            print("...... ",self.name + " have won......")
+            return True
+        return False
+
+
 
--- gameManager.py
+++ gameManager.py
@@ -1,16 +1,34 @@
 from board import board
 
-player1 = board()
-player2 = board()
+print(".......RULES OF GAME.............")
+print("two player game, each plaeyr takes turn after another")
+print("first turn is for placing ships by each player, it takes three arguments of space separated value,\n"
+      "where first two are coordinates of the ship center, and the last is H/V, providing the placement of \nthe ship"
+      "horizontally or vertically (exp: 2 2 H)")
+print("after placement, each player guesses and fires by coordinates in space separated values (exp: 2 2)")
+print(".........Lets have fun ........\n\n\n")
+
+
+player1 = board("player1")
+player2 = board("player2")
 
 turn = 0
 player1turn=True
 
+x1, y1, isHorizontal1 = input("Player1 ship position: x:int,  y:int , H/V: str : ").split()
+while(player2.placeShip(int(x1),int(y1),isHorizontal1=="H") == False):
+    x1, y1, isHorizontal1 = input("Player1 ship position: x:int,  y:int , H/V: str : ").split()
+    #player2.placeShip(int(x1), int(y1), isHorizontal1 == "H")
+
+x2, y2, isHorizontal2 = input("Player2 ship position: x:int,  y:int , H/V: str : ").split()
+while(player1.placeShip(int(x2),int(y2),isHorizontal2=="H") == False):
+    x2, y2, isHorizontal2 = input("Player2 ship position: x:int,  y:int , H/V: str : ").split()
+    #player1.placeShip(int(x2), int(y2), isHorizontal2 == "H")
 
 def setPlayerTurn():
     global turn,player1turn
 
-    print(turn)
+    print("turnCount: ",turn)
     if turn % 2 == 0:
         turn += 1
         player1turn = True
@@ -22,21 +40,43 @@ def setPlayerTurn():
 def firedAtPosition(x,y):
 
     if player1turn:
-
         player1.shotPoint(x,y)
     else:
-
         player2.shotPoint(x, y)
 
-while(True):
+def isThisMoveUSedPreviously(x,y):
+
+    if player1turn:
+        return player1.isThisMoveUSedPreviously(x,y)
+    else:
+        return player2.isThisMoveUSedPreviously(x, y)
+
+def isValidMove(x,y):
+    if (x > 7 or x < 0 or y > 7 or y < 0):
+        return False
+    elif isThisMoveUSedPreviously(x,y):
+        return False
+    return True
+
+def isGameOverBetween(player1,player2):
+    if (player1.opponentDestroyed() or player2.opponentDestroyed()) == True:
+        print("..........Game Over..........")
+        return True
+    return False
+
+while(isGameOverBetween(player1,player2) == False):
     print(setPlayerTurn())
-    print(player1turn, "player1")
     x,y = input("fire at position x y : ").split()
     x,y = int(x),int(y)
-    firedAtPosition(x,y)
+    if isValidMove(x,y) :
+       firedAtPosition(x,y)
+    else:
+        while isValidMove(x,y)==False:
+            print("Invalid Move, please try again, possible cause - out of board position/previously same used position")
+            x, y = input("fire at position x y : ").split()
+            x, y = int(x), int(y)
 
 
 
-print(player1.ownShip)
-print(player2.ownShip)
+
 
